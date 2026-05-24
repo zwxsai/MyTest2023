eb594d0ce240005ce23187db3e3b4160b9210491
wbzhongwx <ly-zhongwenx@dfl.com.cn>
2026-05-24 10:28:36 +0800

    feat:1 添加代码

--- app/src/main/java/com/example/mytest2023/module/kl/PlayerFu.kt
+++ app/src/main/java/com/example/mytest2023/module/kl/PlayerFu.kt
@@ -7,75 +7,74 @@ import android.util.Log
  *Describer:
  */
 open class PlayerFu( //类默认都是不能被继承的，加上open就能被继承
-	_id: Int, _name: String, var age: Int = 32
+    _id: Int, _name: String, var age: Int = 32
 ) {
-	//operator 运算符 可以对对象用 +号了
-	operator fun component1() = age + 45
+    //operator 运算符 可以对对象用 +号了
+    operator fun component1() = age + 45
 
-	var id = _id
-		//		get() {
-//			var str: String = "ABC"
-//			return str.length
-//		}
-		set(value) {
-			var str: String = "ABC"
-			field = str.length + value //field就是本身
-		}
+    var id = _id
+        //		get() {
+        //			var str: String = "ABC"
+        //			return str.length
+        //		}
+        set(value) {
+            var str: String = "ABC"
+            field = str.length + value //field就是本身
+        }
 
-	var name = _name
-		//		get() {
-//			return name + "56"
-//		}
-		set(value) {
-			name = value + "567"
-		}
-
-
-
-	init {
-		var tr = 4
-		var name2 = name
-	}
-
-	//次构造函数
-	constructor(id2: Int) : this(id2, _name = "zhong") {
-		this.id = id2 + 10
-	}
+    var name = _name
+        //		get() {
+        //			return name + "56"
+        //		}
+        set(value) {
+            name = value + "567"
+        }
 
 
-//	override fun toString(): String {
-//		return "Player2(id=$id, name='$name',age=$age, )"
-//	}
+    init {
+        var tr = 4
+        var name2 = name
+    }
 
-	override fun toString(): String {
-		return "Player2(id=$id, name='$name',age=$age, )"
-	}
+    //次构造函数
+    constructor(id2: Int) : this(id2, _name = "zhong") {
+        this.id = id2 + 10
+    }
 
-	open fun dodo() {
-		if (::work.isInitialized) {////函数引用
-			Log.e("kotlin", "dodo: " + work)
-		}
 
-	}
+    //	override fun toString(): String {
+    //		return "Player2(id=$id, name='$name',age=$age, )"
+    //	}
 
-	open fun getStr(): String {
-		return "AAB"
-	}
+    override fun toString(): String {
+        return "Player2(id=$id, name='$name',age=$age, )"
+    }
 
-	lateinit var work: String  //延时初始化
+    open fun dodo() {
+        if (::work.isInitialized) { ////函数引用
+            Log.e("kotlin", "dodo: " + work)
+        }
 
-	val config by lazy { test1() } //懒惰加载 几秒后才会执行   lazy只适用于val对象，对于var对象，需要使用lateinit
-	fun test1(): String {
-		Log.e("kotlin", "test1: loading")
-		if (age == 3) {
+    }
 
-		}
-		return "ABC"
-	}
+    open fun getStr(): String {
+        return "AAB"
+    }
+
+    lateinit var work: String  //延时初始化
+
+    val config by lazy { test1() } //懒惰加载 几秒后才会执行   lazy只适用于val对象，对于var对象，需要使用lateinit
+    fun test1(): String {
+        Log.e("kotlin", "test1: loading")
+        if (age == 3) {
+
+        }
+        return "ABC"
+    }
 }
 
 //调用顺序
 //1、主构造函数的参数声明
 //2、类级别的参数赋值
 //3、init()方法的赋值和函数调用  ，注init()方法需要写在参数后面
-//4、次构造函数的赋值和函数调用
+//4、constructor次构造函数的赋值和函数调用
--- app/src/test/java/com/example/mytest2023/module/kl/KotlinActivityTest.kt
+++ app/src/test/java/com/example/mytest2023/module/kl/KotlinActivityTest.kt
@@ -1,7 +1,9 @@
 package com.example.mytest2023.module.kl
 
 import android.util.Log
+import org.junit.Assert
 import org.junit.Test
+
 
 /**
  * Created by 钟文祥 on 2026/1/23.
@@ -14,7 +16,19 @@ class KotlinActivityTest {
         Log.e("lgddfg", "onClick2: 33")
     }
 
+
     @Test
     fun onClick() {
+        val sdf = 3
+        val dfgdf = 5
+        val dtr = true
+        Assert.assertEquals(false, sdf + dfgdf != 9) //断言验证结果是否正确 是否等于8
+//        Assert.assertEquals(true, sdf + dfgdf > 9)
+
     }
+
+    data class TestA(
+        val a: Int,
+        val b: Boolean
+    )
 }
